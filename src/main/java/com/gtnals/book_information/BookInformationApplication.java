49605439551948861f11aeaaa5c92688a4f9e390
gtnals <gtnals9720@gmail.com>
2021-12-10 09:55:52 +0900

    첫 프로젝트 파일 업로드

---
+++ src/main/java/com/gtnals/book_information/BookInformationApplication.java
@@ -0,0 +1,13 @@
+package com.gtnals.book_information;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookInformationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtnals/book_information/BookInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtnals.book_information;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
